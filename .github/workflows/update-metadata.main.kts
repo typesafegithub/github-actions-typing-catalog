1f94a9e94fb4bb38dffe42f217fe1735161af624
Piotr Krzeminski <git@krzeminski.it>
2024-01-04 22:09:42 +0100

    Use Kotlin logic step to inline the script

--- .github/workflows/update-metadata.main.kts
+++ .github/workflows/update-metadata.main.kts
@@ -1,11 +1,19 @@
 #!/usr/bin/env kotlin
 @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.9.0")
+@file:DependsOn("it.krzeminski:snakeyaml-engine-kmp-jvm:2.7.1")
+@file:DependsOn("org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r")
 
 import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
+import io.github.typesafegithub.workflows.annotations.ExperimentalKotlinLogicStep
 import io.github.typesafegithub.workflows.domain.RunnerType
 import io.github.typesafegithub.workflows.dsl.workflow
 import io.github.typesafegithub.workflows.domain.triggers.Push
 import io.github.typesafegithub.workflows.yaml.writeToFile
+import org.snakeyaml.engine.v2.api.Dump
+import org.snakeyaml.engine.v2.api.DumpSettings
+import org.snakeyaml.engine.v2.common.ScalarStyle
+import java.io.File
+import org.eclipse.jgit.api.Git
 
 workflow(
     name = "Update metadata",
@@ -24,13 +32,57 @@ workflow(
                 git config user.name "github-actions[bot]"
             """.trimIndent()
         )
-        run(
-            name = "Run generation logic",
-            command = "scripts/updateMetadata.main.kts",
-        )
+        @OptIn(ExperimentalKotlinLogicStep::class)
+        run(name = "Run generation logic") {
+            removeMetadataFiles()
+            generateMetadataFiles()
+            commitChanges()
+        }
         run(
             name = "Push new commit",
             command = "git push",
         )
     }
 }.writeToFile()
+
+fun removeMetadataFiles() {
+    File("typings").walk()
+        .filter { it.name == "metadata.yml" }
+        .forEach { it.delete() }
+}
+
+fun generateMetadataFiles() {
+    File("typings").walk()
+        .filter { it.isActionRootDir() }
+        .forEach { actionRootDir ->
+            val versionsWithTypings = actionRootDir.listFiles()
+                .filter { it.isDirectory }
+                .map { it.name }
+                .sortedBy { it.removePrefix("v") }
+            writeToMetadataFile(actionRootDir, versionsWithTypings)
+        }
+}
+
+fun commitChanges() {
+    Git.open(File(".")).apply {
+        add().addFilepattern("typings/").call()
+
+        if (status().call().hasUncommittedChanges()) {
+            commit().setMessage("Update metadata").call()
+        }
+    }
+}
+
+fun writeToMetadataFile(actionRootDir: File, versionsWithTypings: List<String>) {
+    val structureToDump = mapOf(
+        "versionsWithTypings" to versionsWithTypings
+    )
+    val dumpSettings = DumpSettings.builder()
+        .setDefaultScalarStyle(ScalarStyle.DOUBLE_QUOTED)
+        .build()
+    val dump = Dump(dumpSettings)
+    val yamlAsString = dump.dumpToString(structureToDump)
+    actionRootDir.resolve("metadata.yml").writeText(yamlAsString)
+}
+
+fun File.isActionRootDir() = path.split("/").filter { it.isNotBlank() }.size == 3
