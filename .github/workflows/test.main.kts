ed157faa6361fd6c5d2be5d19a855a45d0c0f58b
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-08-19 08:03:20 +0200

    Configure CI to test Kotlin workflows (#27)

---
+++ .github/workflows/test.main.kts
@@ -0,0 +1,64 @@
+#!/usr/bin/env kotlin
+@file:Repository("https://repo.maven.apache.org/maven2/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:2.3.0")
+
+@file:Repository("https://bindings.krzeminski.it")
+@file:DependsOn("actions:checkout:v4")
+
+import io.github.typesafegithub.workflows.actions.actions.Checkout
+import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
+import io.github.typesafegithub.workflows.domain.triggers.PullRequest
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.workflow
+
+workflow(
+    name = "Test",
+    on = listOf(
+        Push(branches = listOf("main")),
+        PullRequest(),
+    ),
+    sourceFile = __FILE__,
+) {
+    job(
+        id = "build_kotlin_scripts",
+        name = "Build Kotlin scripts",
+        runsOn = UbuntuLatest,
+    ) {
+        uses(action = Checkout())
+        run(
+            command = """
+            find -name *.main.kts -print0 | while read -d ${'$'}'\0' file
+            do
+                echo "Compiling ${'$'}file..."
+                kotlinc -Werror -Xallow-any-scripts-in-source-roots -Xuse-fir-lt=false "${'$'}file"
+            done
+            """.trimIndent()
+        )
+    }
+
+
+    job(
+        id = "workflows_consistency_check",
+        name = "Run consistency check on all GitHub workflows",
+        runsOn = UbuntuLatest,
+    ) {
+        uses(action = Checkout())
+        run(command = "cd .github/workflows")
+        run(
+            name = "Regenerate all workflow YAMLs",
+            command = """
+            find -name "*.main.kts" -print0 | while read -d ${'$'}'\0' file
+            do
+                if [ -x "${'$'}file" ]; then
+                    echo "Regenerating ${'$'}file..."
+                    (${'$'}file)
+                fi
+            done
+            """.trimIndent(),
+        )
+        run(
+            name = "Check if some file is different after regeneration",
+            command = "git diff --exit-code .",
+        )
+    }
+}
